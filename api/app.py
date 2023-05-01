c31dda7deb3cfe6719274b715571dae84924a5a0
Sebastian Dunn <sebastiancdunn@byui.edu>
2023-05-01 07:42:49 -0700

    Algo base working

--- api/app.py
+++ api/app.py
@@ -1,43 +1,33 @@
-# Run with python -m flask run
-from flask import Flask, render_template, request, redirect
-import pandas as pd
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from flask_talisman import Talisman
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth, CacheHandler
-from config import CLIENT_ID, CLIENT_SECRET
+from spotipy.oauth2 import SpotifyOAuth
+from config import CLIENT_ID, CLIENT_SECRET, APP_KEY, MONGODB_USER, MONGODB_PASS
 import json
 import pymongo
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
+app = Flask(__name__)
+app.secret_key = APP_KEY
+
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_SECURE'] = True
+Session(app)
+talisman = Talisman(app)
+
 # Connect to MongoDB Atlas cluster
-username = quote_plus("axolmain")
-password = quote_plus("jxpxwWsZFP3vo5DT")
+username = quote_plus(MONGODB_USER)
+password = quote_plus(MONGODB_PASS)
 MONGODB_URI = f"mongodb+srv://{username}:{password}@spotifyswiperfree.urhcq77.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(MONGODB_URI)
 db = client.spotifyswiperfree
 collection = db.responses
 
-class MongoDBCacheHandler(CacheHandler):
-    def __init__(self, db):
-        self.db = db
-
-    def get_cached_token(self):
-        token = self.db.cacheHandler.find_one()
-        return token if token else None
-
-    def save_token_to_cache(self, token_info):
-        self.db.cacheHandler.replace_one({}, token_info, upsert=True)
-
-
-app = Flask(__name__)
-
 # Set up Spotify authentication
 # my_env_var =
-# Set up Spotify authentication with custom cache handler
-cache_handler = MongoDBCacheHandler(db)
-sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="https://spotify-swiper.vercel.app/redirect", scope="user-top-read", cache_handler=cache_handler)
-
-# maybe setup as an auth header 
+sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://31.220.108.226:4200/redirect", scope="user-top-read")
 
 @app.route('/')
 def hello():
@@ -53,16 +43,38 @@ def login():
 def redirected_name():
     # Get the authorization code from the Spotify redirect
     code = request.args.get('code')
+    print("some code", code)
 
     # Exchange the code for an access token
-    token_info = sp_oauth.get_access_token(code)
+    token_info = sp_oauth.get_access_token(code, check_cache=False)
     access_token = token_info['access_token']
+    session['access_token'] = access_token
+
+    # Create a new Spotipy client with the access token
+    sp = spotipy.Spotify(auth=access_token)
+    print("this is a code", code)
+    print("sp thing", sp)
+
+    # Get the user's ID from the Spotify API response
+    user_id = sp.current_user()["id"]
+    session['user_id'] = user_id
+    print("user_id at start", user_id)
+
+    return render_template('user.html', user_id=user_id, access_token=access_token)
+
+@app.route('/give_data', methods=['POST'])
+def give_data():
+    access_token = session.get('access_token')
+    user_id = session.get('user_id')
+
+    print(user_id)
+
+    if not access_token:
+        return redirect('/')
 
     # Create a new Spotipy client with the access token
     sp = spotipy.Spotify(auth=access_token)
 
-    # Get the user's ID from the Spotify API response
-    user_id = sp.current_user()["id"]
 
     # Get the user's top artists and tracks
     top_tracks_short = sp.current_user_top_tracks(time_range='short_term', limit=20)
@@ -77,6 +89,8 @@ def redirected_name():
     blist4 = [top_tracks_medium['items'][song]['album']['id'] for song in range(len(top_tracks_medium['items']))]
     blist5 = [top_artists_short['items'][song]['id'] for song in range(len(top_artists_short['items']))]
     blist6 = [top_artists_medium['items'][song]['id'] for song in range(len(top_artists_medium['items']))]
+    # all genres of the songs/albums put into a list
+    blist7 = [top_tracks_short['items'][song]['album']['genres'] for song in range(len(top_tracks_short['items']))]
     audio_features_short = sp.audio_features(tracks=blist1)
     audio_features_medium = sp.audio_features(tracks=blist2)
 
@@ -113,9 +127,16 @@ def redirected_name():
             for artist in top_artists_medium["items"]
         ],
     }
+    print("user_id almost end", user_id)
 
     # Save the user's information to a new MongoDB document
-    collection.insert_one({"user_id": user_id, "user_info": user_info})
+    if collection.find_one({"user_id": user_id}):
+        collection.update_one({"user_id": user_id}, {"$set": {"user_info": user_info}})
+    else:
+        collection.insert_one({"user_id": user_id, "user_info": user_info})
+    print("user_id at end", user_id)
+    
+    return render_template('display_data.html', user_info=user_info)
 
-    # Return the top artists to the user
-    return render_template('user.html', top_artists=blist1)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4200)
